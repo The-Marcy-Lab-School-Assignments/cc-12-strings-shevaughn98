a18325fc2794b51703828eb133a3e7dacd553c91
shevaughn98 <shevaughnlegall@gmail.com>
2021-11-03 14:55:22 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//write your code here
+function reverseString(str){
+    let reversed = ""
+    for(let i = str.length-1; i >= 0; i-- ){
+        reversed+= str[i]; 
+    }
+    return reversed
+}
+console.log(reverseString("hello"))
